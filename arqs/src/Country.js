75bec157867c04c746fa15d7ef0421a13c23031c
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-10-31 15:49:59 -0300

    template country error

--- arqs/src/Country.js
+++ arqs/src/Country.js
@@ -5,16 +5,56 @@ function objectKeys(data) {
   const regex_year = new RegExp(/(2017)/g);
   return (
     Object.keys(data.cases).filter((value) => {
-    if (value.match(regex_year)){
-      return true
-    } else {
-      return false
-    }
+      if (value.match(regex_year)){
+        return true
+      } else {
+        return false
+      }
     })
   )
 }
 
+function cases(country, api){
+  let dataCountry = [];
+  for (let i in api){
+    dataCountry.push(api[i][country])
+  }
+  const result = dataCountry.slice(-1)[0];
+  return result;
+}
+
+function ClickCountry(data){
+  return(
+    <div className="container d-flex justify-content-center">
+      <div className="row d-flex align-items-center">
+        <div className="col">
+          <p>Segundo a Onu até o momento, 38 países e territórios confirmaram a transmissão local do vírus zika na
+          Região das Américas desde 2015.</p>
+          <p> No <strong>{data.country}</strong> somente em 2017 foram registrados:</p>
+          <div id="show-data">
+          <p className="mb-0"> {data.autochthonous_cases_confirmed} casos contraídos no país.</p>
+          <p className="mb-0"> {data.imported_cases} casos contraídos no exterior.</p>
+          <p className="mb-0"> {data.deaths} mortes.</p>
+          <p className="mb-0"> {data.confirmed_congenital} crianças que nasceram com o zika vírus</p>
+          <p className="mb-0">{data.congenital_probable} casos com probabilidade de vir a nascer com o vírus. </p>
+          </div>
+          <h4 className="mt-4"><a class="link-externo" href="http://www.who.int/emergencies/diseases/zika/en">Zika vírus no mundo </a></h4>
+        </div>
+        <div className="col">
+          <a className="link-externo" href="https://www.unicef.org/brazil/pt/activities_32722.html"><img className="img-prev" src="dist/img/prev3.png"></img></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 class Country extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {objCountries: null,
+    }
+  }
 
 componentDidMount(){
     const objCountries = [];
@@ -27,33 +67,16 @@ componentDidMount(){
       .then(res => res.json())
       .then(data => {objectKeys(data).forEach((week) => {
       objCountries.push(data.cases[week]);
+      this.setState({objCountries: objCountries})
       })
-        console.log(objCountries)
-      })
-
-      function cases(country){
-        let dataCountry = [];
-        for (let i in objCountries){
-          dataCountry.push(objCountries[i][country])
-        }
-
-        const result =
-          $(document).ready(function(){
-            $(dataCountry).get(-1)
-          }
-
-        return result;
-      }
-
-      const brasil = cases('bra');
-      const chile = cases('chl');
-      const bolivia = cases('bol');
+    })
 }
 
   render() {
     return (
       <div>
-        turu bom
+        {ClickCountry(cases('bra', this.state.objCountries))}
+
       </div>
     );
   }
